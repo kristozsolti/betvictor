68c9b37e13773bf1892b2cae0d99704459e43314
Zsolt Kristó <kristo.zsolti@arxall.com>
2024-06-25 08:55:41 +0300

    Update project structure

---
+++ repository/src/test/java/com/betvictor/loremipsum/repository/RepositoryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.betvictor.loremipsum.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class RepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
