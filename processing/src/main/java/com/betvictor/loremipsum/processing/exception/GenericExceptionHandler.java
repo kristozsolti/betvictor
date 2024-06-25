57a352636db80ca31837e107682d0ebd5fbf074d
Zsolt Kristó <kristo.zsolti@arxall.com>
2024-06-25 13:44:38 +0300

    Create exception and handler

---
+++ processing/src/main/java/com/betvictor/loremipsum/processing/exception/GenericExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.betvictor.loremipsum.processing.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GenericExceptionHandler {
+//    TODO Handle exceptions application-wise
+}
