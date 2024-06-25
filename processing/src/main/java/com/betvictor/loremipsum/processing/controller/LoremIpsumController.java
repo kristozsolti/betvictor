8fde6e8827349e9d9a588ed21e51d40ae6698ceb
Zsolt Kristó <kristo.zsolti@arxall.com>
2024-06-25 13:44:52 +0300

    Create controller

---
+++ processing/src/main/java/com/betvictor/loremipsum/processing/controller/LoremIpsumController.java
@@ -0,0 +1,24 @@
+package com.betvictor.loremipsum.processing.controller;
+
+import com.betvictor.loremipsum.processing.dto.LoremIpsumResponse;
+import com.betvictor.loremipsum.processing.service.LoremIpsumService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/betvictor")
+public class LoremIpsumController {
+    private final LoremIpsumService loremIpsumService;
+
+    @GetMapping(path = "/text")
+    public ResponseEntity<LoremIpsumResponse> generateDummyText(@RequestParam("p") int numOfParagraphs,
+                                                                @RequestParam("l") String paragraphLength) {
+        return loremIpsumService.generateDummyText(numOfParagraphs, paragraphLength);
+    }
+
+}
